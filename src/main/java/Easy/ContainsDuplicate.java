31a3261d275c4bc15ff3140f9ced60e9c1e547e1
Gleb Lipatov <it.lipatov@gmail.com>
2023-06-05 20:23:54 +0300

    fix1

--- src/main/java/Easy/ContainsDuplicate.java
+++ src/main/java/Easy/ContainsDuplicate.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 public class ContainsDuplicate {
     public boolean solution(int[] nums) {
         HashSet<Integer> numbers = new HashSet<>();
+
         int length = nums.length;
         for (int i = 0; i < length; i++) {
             if (numbers.contains(nums[i])) return true;
